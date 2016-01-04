d7bb31d5cf758cd26b3fff91373bace0da907863
nickolanack <nickblackwell82@gmail.com>
2016-01-04 15:31:10 -0800

    added server ajax option to display graph results with extended details. also removed caching.

--- newspoverty.onrender.php
+++ newspoverty.onrender.php
@@ -2,7 +2,7 @@
 /**
  * Copy the following into Timeline onRender script input (do not include the <script> tags...)
  */
- var erraSpan=container.appendChild(new Element('span', {'class':'era-s'}));
+var erraSpan=container.appendChild(new Element('span', {'class':'era-s'}));
  var barBack=erraSpan.appendChild(new Element('div', {'class':'era-bar bk'}));
  var graphBar=erraSpan.appendChild(new Element('div',{'class':'timeline-graph'}))
  var bar=erraSpan.appendChild(new Element('div', {'class':'era-bar'}));
@@ -70,11 +70,11 @@
      });
 
      new UIPopover(pin, {anchor:UIPopover.AnchorTo(['top']),
- 				title:'',
- 				description:event.label//,
- 				//hideDelay:500,
- 				//margin:50
- 			});
+                title:'',
+                description:event.label//,
+                //hideDelay:500,
+                //margin:50
+            });
 
  });
 
@@ -86,61 +86,59 @@ Behavior('graph');
 
 
 
- (new TimelineQuery('get_timeline_graph', {})).cache({
-     expire:5*3600
- }).addEvent('success',function(resp){
+ (new TimelineQuery('get_timeline_graph', {showDates:true})).addEvent('success',function(resp){
      var data=resp.values;
 
 
-     var hintBar=new UIGraph(graphBar, data, {
+     var hintBar=new UIGraph(graphBar, data.map(function(d){return d.count;}), {
                 lineTemplate:UIGraph.UnitStepBarsTemplate,
                  //lineTemplate:UIGraph.LineTemplate,
- 				title:"",
- 				height:26,
- 				width:100,
+                title:"",
+                height:26,
+                width:100,
                 widthUnit:'%',
- 				padding:0,
- 				lineColor: '#CCCCCC',
- 				fillGradient:true
- 			});
+                padding:0,
+                lineColor: '#CCCCCC',
+                fillGradient:true
+            });
 
-     var detailBar=new UIGraph(graphBarDetail, data, {
+     var detailBar=new UIGraph(graphBarDetail, data.map(function(d){return d.count;}), {
                 lineTemplate:UIGraph.UnitStepBarsTemplate,
                  //lineTemplate:UIGraph.LineTemplate,
- 				title:"Number of changes since 2008",
- 				height:77,
- 				width:100,
+                title:"Number of changes since 2008",
+                height:77,
+                width:100,
                 widthUnit:'%',
- 				padding:0,
- 				lineColor: 'black',
+                padding:0,
+                lineColor: 'black',
                 fillGradient:true,
                 highlightTemplate:UIGraph.UnitStepBarsHighlighter
 
- 			});
+            });
      detailBar.addEvent('click',function(data){
 
-    	 console.log(data);
-    	 timeline.setValue([data.x, data.x+1]);
+         console.log(data);
+         timeline.setValue([data.x, data.x+1]);
      });
 
      //adding a custom popover that follows the cursor.
      var popover=null;
      detailBar.addEvent('mouseover',function(d){
 
-    	   if(d.y>0){
-        	   if(!popover){
-        		    popover=new UIPopover(detailBar.canvas);
-        		    popover.show();
+           if(d.y>0){
+               if(!popover){
+                    popover=new UIPopover(detailBar.canvas);
+                    popover.show();
                }
 
-        	   popover.setText(d.y+" outlet"+(d.y==1?"":"s"));
-    	   }else{
-    		   if(popover){
-    			    popover.hide();
-    			    popover.detach();
-    			    popover=null;
+               popover.setText(d.y+" outlet"+(d.y==1?"":"s"));
+           }else{
+               if(popover){
+                    popover.hide();
+                    popover.detach();
+                    popover=null;
 
-        	   }
+               }
 
            }
 
@@ -155,136 +153,134 @@ Behavior('graph');
      detailBar.titleEl.appendChild((function(){
 
 
- 				var span=new Element('span', {'class':'timeline-opts'});
+                var span=new Element('span', {'class':'timeline-opts'});
 
- 			    var checkboxes=[];
+                var checkboxes=[];
 
- 			    var allBoxesAreChecked=function(){
- 			       for(var i=0;i<checkboxes.length;i++){
- 			    	    if(!checkboxes[i].checked){
- 	 			    	    return false;
- 			    	    }
- 	 			   }
- 	 			   return true;
- 	 			};
- 	 			var someBoxedAreChecked=function(){
-     	 			 for(var i=0;i<checkboxes.length;i++){
-     			    	    if(checkboxes[i].checked){
-     	 			    	    return true;
-     			    	    }
-     	 			}
-     	 			return false;
+                var allBoxesAreChecked=function(){
+                   for(var i=0;i<checkboxes.length;i++){
+                        if(!checkboxes[i].checked){
+                            return false;
+                        }
+                   }
+                   return true;
+                };
+                var someBoxedAreChecked=function(){
+                     for(var i=0;i<checkboxes.length;i++){
+                            if(checkboxes[i].checked){
+                                return true;
+                            }
+                    }
+                    return false;
 
- 	 	 		};
- 	 			var allCheckedOptions=function(){
- 	 			    var options=[];
- 	 				for(var i=0;i<checkboxes.length;i++){
- 			    	    if(checkboxes[i].checked){
- 	 			    	    options.push(checkboxes[i].getAttribute('data-filter'));
- 	 			    	    }
- 	 			   }
- 	 			   return options;
+                };
+                var allCheckedOptions=function(){
+                    var options=[];
+                    for(var i=0;i<checkboxes.length;i++){
+                        if(checkboxes[i].checked){
+                            options.push(checkboxes[i].getAttribute('data-filter'));
+                            }
+                   }
+                   return options;
 
- 	 		    };
+                };
 
- 	 		    var cacheData={};
+                var cacheData={};
 
- 	 		    var makeCacheKey=function(){
- 	 		    	var key= allCheckedOptions().join('-').replace(/[;\s]/g,'');
- 	 		    	console.log(key);
- 	 		    	return key;
- 	 	 		};
+                var makeCacheKey=function(){
+                    var key= allCheckedOptions().join('-').replace(/[;\s]/g,'');
+                    console.log(key);
+                    return key;
+                };
 
- 	 		  (new AjaxControlQuery(CoreAjaxUrlRoot,'distinct_attribute_value_list', {
-				    plugin:'Attributes',
-				    table:'newsAttributes',
-				    field:'transitionType'
-	 			})).cache({
+              (new AjaxControlQuery(CoreAjaxUrlRoot,'distinct_attribute_value_list', {
+                    plugin:'Attributes',
+                    table:'newsAttributes',
+                    field:'transitionType'
+                })).cache({
                                      expire:5*3600
                                 }).addEvent('success',function(resp){
 
-	 					console.log(resp);
-	 				    var options=resp.values;
+                        console.log(resp);
+                        var options=resp.values;
 
 
                 var countCbx=0;
                 span.appendChild(new Element('br'));
- 			    Array.each(options,function(opt){
+                Array.each(options,function(opt){
 
                   if((typeof opt)!='string'||opt.length==0){
                     return; //continue;
                   }
 
                   countCbx++;
- 			      var checkbox=new Element('input',{type:'checkbox', checked:true, 'data-filter':opt});
- 			      var label=new Element('label', {title:opt});
- 			      span.appendChild(label);
+                  var checkbox=new Element('input',{type:'checkbox', checked:true, 'data-filter':opt});
+                  var label=new Element('label', {title:opt});
+                  span.appendChild(label);
                   if(countCbx%3==0){
                     span.appendChild(new Element('br'));
                   }
- 			      label.appendChild(checkbox);
+                  label.appendChild(checkbox);
                   label.appendChild(new Element('span',{html:opt}));
- 			      checkboxes.push(checkbox);
- 			      checkbox.addEvent('click', function(){
+                  checkboxes.push(checkbox);
+                  checkbox.addEvent('click', function(){
 
 
- 			    	    var cacheKey=makeCacheKey();
- 			    	    if(allBoxesAreChecked()){
+                        var cacheKey=makeCacheKey();
+                        if(allBoxesAreChecked()){
 
- 			    	    	detailBar.setData(data);
- 	 			    		hintBar.setData(data);
+                            detailBar.setData(data.map(function(d){return d.count;}));
+                            hintBar.setData(data.map(function(d){return d.count;}));
 
- 	 			        }else if((typeof cacheData[cacheKey])!='undefined'){
+                        }else if((typeof cacheData[cacheKey])!='undefined'){
 
- 	 			        	    console.log('used cached data');
- 	 			    	    	detailBar.setData(cacheData[cacheKey]);
- 	 	 			    		hintBar.setData(cacheData[cacheKey]);
+                                console.log('used cached data');
+                                detailBar.setData(cacheData[cacheKey].map(function(d){return d.count;}));
+                                hintBar.setData(cacheData[cacheKey].map(function(d){return d.count;}));
 
- 	 	 			    }else {
+                        }else {
 
- 			    	       if(someBoxedAreChecked()){
+                           if(someBoxedAreChecked()){
 
-         			    	(new TimelineQuery('get_timeline_graph', {
+                            (new TimelineQuery('get_timeline_graph', {
+                                showDates:true,
+                                filter:AttributeFilter.JoinFilter('newsAttributes', allCheckedOptions().map(function(opt){
 
-         			    		filter:AttributeFilter.JoinFilter('newsAttributes', allCheckedOptions().map(function(opt){
+                                                    return {
+                                                        field:'transitionType',
+                                                        comparator:'equalTo',
+                                                        value:opt,
+                                                    }
 
-	            							    	return {
-	            							        	field:'transitionType',
-	            							            comparator:'equalTo',
-	            							            value:opt,
-	            							        }
+                                                }))
 
-		            							}))
+                                })).addEvent('success',function(resp){
 
-         	 			    	})).cache({
-                                     expire:5*3600
-                                }).addEvent('success',function(resp){
+                                    cacheData[cacheKey]=resp.values;
+                                    detailBar.setData(resp.values.map(function(d){return d.count;}));
+                                    hintBar.setData(resp.values.map(function(d){return d.count;}));
 
-             	 			    	cacheData[cacheKey]=resp.values;
-         	 			    		detailBar.setData(resp.values);
-         	 			    		hintBar.setData(resp.values);
+                            }).execute();
 
-         	 			    }).execute();
+                           }else{
 
- 			    	       }else{
+                              //do nothing!
+                              detailBar.setData([]);
+                              hintBar.setData([]);
 
- 			    	    	  //do nothing!
- 			    	    	  detailBar.setData([]);
- 			    	    	  hintBar.setData([]);
+                           }
 
- 	 			    	   }
+                       }
 
- 			    	   }
+                  });
 
- 	 			  });
+                });
 
- 				});
+        }).execute();
 
- 		}).execute();
+        return span;
 
- 		return span;
-
- 	})());
+    })());
 
 
 
